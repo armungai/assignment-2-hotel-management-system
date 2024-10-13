4a6c567245f72eb4e29d8b760f8598fc8c7cc950
armungai <160440785+armungai@users.noreply.github.com>
2024-10-14 10:52:52 +1100

    Initial commit - windows forms project setup

---
+++ hotel-booking-system-assignment-2/Form1.cs
@@ -0,0 +1,10 @@
+namespace hotel_booking_system_assignment_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
